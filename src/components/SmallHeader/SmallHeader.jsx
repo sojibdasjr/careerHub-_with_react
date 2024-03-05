6225c797c799c385b01fb0e267d1808aa6c71533
Sojib Das <contact.sojibdas@gmail.com>
2024-03-05 23:11:32 +0600

    change logo careerhub

--- src/components/SmallHeader/SmallHeader.jsx
+++ src/components/SmallHeader/SmallHeader.jsx
@@ -7,7 +7,11 @@ const SmallHeader = () => {
   return (
     <div className="lg:flex  bg-green-400 justify-around  items-center px-5 ">
       <h1 className=" text-white items-start ">
-        world best job portal is careerhub
+        world best job portal is{" "}
+        <span>
+          <span className="bg-purple-400 p-0.5 rounded-s">career</span>
+          <span className="bg-sky-400 p-0.5 rounded-e">hub</span>
+        </span>
       </h1>
       <div className="flex gap-2">
         <a href="#" className="flex items-center">
