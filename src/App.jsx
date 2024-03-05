ef685793ec8c7d8bf1a9a8ada5d05edd575e5b1a
Sojib Das <contact.sojibdas@gmail.com>
2024-03-05 23:02:39 +0600

    creact secondery header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import SmallHeader from "./components/SmallHeader/SmallHeader";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1 className="bg-slate-500 text-red-500">React CareerHub</h1>
+      <SmallHeader />
     </>
   );
 }
---
+++ src/components/SmallHeader/SmallHeader.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import { SiMinutemailer } from "react-icons/si";
+import { MdLocationOn } from "react-icons/md";
+
+const SmallHeader = () => {
+  return (
+    <div className="lg:flex  bg-green-400 justify-around  items-center px-5 ">
+      <h1 className=" text-white items-start ">
+        world best job portal is careerhub
+      </h1>
+      <div className="flex gap-2">
+        <a href="#" className="flex items-center">
+          <SiMinutemailer className="text-white" />{" "}
+          <small className="text-white">contact@careerhub.com</small>
+        </a>
+        <a href="#" className="flex items-center">
+          <MdLocationOn className="text-white" />
+          <small className="text-white">contact@careerhub.com </small>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default SmallHeader;
