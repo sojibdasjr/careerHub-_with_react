eb44def985e8a6f38f823e978adbfe72bbb5eb38
Sojib Das <contact.sojibdas@gmail.com>
2024-04-05 21:22:15 +0600

    add more button

--- src/components/Pages/Statistics.jsx
+++ src/components/Pages/Statistics.jsx
@@ -17,6 +17,19 @@ const Statistics = () => {
         >
           Statistics
         </button>
+        <button
+          className=" text-center text-yellow-400  
+      bg-black 
+      py-2 px-5  rounded
+
+      hover:bg-yellow-600
+      hover:text-black
+  
+      duration-300
+      "
+        >
+          Statistics
+        </button>
       </div>
     </div>
   );
