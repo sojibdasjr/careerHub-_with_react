c911bc8aceea242452cd1e2c7d82a8e84a474e3f
Sojib Das <contact.sojibdas@gmail.com>
2024-04-06 20:11:08 +0600

    change button style

--- src/components/Pages/Statistics.jsx
+++ src/components/Pages/Statistics.jsx
@@ -4,19 +4,7 @@ const Statistics = () => {
   return (
     <div className="bg-white py-10">
       <div className="flex justify-center items-center      ">
-        <button
-          className=" text-center text-yellow-400  
-      bg-black 
-      py-2 px-5  rounded
-
-      hover:bg-yellow-600
-      hover:text-black
-  
-      duration-300
-      "
-        >
-          Statistics
-        </button>
+        <button>Statistics</button>
       </div>
     </div>
   );
