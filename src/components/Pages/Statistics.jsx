e0977044ae1ecb207bbbbb0ad9163ea8e9a43892
Sojib Das <contact.sojibdas@gmail.com>
2024-04-07 17:27:24 +0600

    delete button

--- src/components/Pages/Statistics.jsx
+++ src/components/Pages/Statistics.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Statistics = () => {
   return (
     <div className="bg-white py-10">
-      <div className="flex justify-center items-center      ">
+      <div className="flex justify-center items-center">
         <button>Statistics</button>
       </div>
     </div>
